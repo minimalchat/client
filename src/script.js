122e2a52e8c46107b529f902d3ede1c872043ced
Matthew Mihok <4D.69.68.6F.6B@gmail.com>
2016-09-24 20:27:26 -0400

    fixed position and emit on enter but not shift-enter

--- src/script.js
+++ src/script.js
@@ -8,38 +8,35 @@
   const ReactRedux = w.ReactRedux || false;
   // const $ = w.jQuery || false;
   const io = w.io || false;
+  const socketPath = 'http://localhost:8000';
+  // let socket;
 
 
   // Constants
 
+  // KeyCode Constants
+  const KEY_ENTER = 13;
+
+  // React Constants
   const UI_OPEN = 'UI_OPEN';
   const UI_CLOSE = 'UI_CLOSE';
+  const UI_SOFT_ENTER = 'UI_SOFT_ENTER';
+  const UI_HARD_ENTER = 'UI_HARD_ENTER';
+  const UI_STYLE_MESSANGER = 'UI_STYLE_MESSANGER';
+  const UI_STYLE_FLOAT = 'UI_STYLE_FLOAT';
+  const UI_STYLE_SIDEBAR = 'UI_STYLE_SIDEBAR';
 
-
-  // Functions
-
-  // Successful connection
-  const socketConnected = function socketConnected () { };
-  // Disconnected
-  const socketDisconnected = function socketDisconnected () { };
-  // Successful re-connected
-  const socketReconnected = function socketReconnected () { };
-  // Attempting to re-connect
-  const socketReconnecting = function socketReconnecting () { };
-
-  // Timeout either connecting or re-connecting
-  const socketTimeout = function socketTimeout () { };
-  // Error when connecting or re-connecting
-  const socketConnectionError = function socketConnectionError () { };
-
+  const CHAT_CONNECTED = 'CHAT_CONNECTED';
+  const CHAT_DISCONNECTED = 'CHAT_DISCONNECTED';
 
   // State
   const { combineReducers, createStore } = Redux;
   const { connect } = ReactRedux;
 
   // UI based actions (open, close)  will go through the uiReducer
-  const uiInitialState = { };
+  const uiInitialState = {};
   const uiReducer = function UIReducer (state = uiInitialState, action) {
+    console.log('UI', action.type);
     switch (action.type) {
       case UI_CLOSE:
         return Object.assign({}, state, {
@@ -56,6 +53,7 @@
     messages: []
   };
   const chatReducer = function ChatReducer (state = chatInitialState, action) {
+    console.log('CHAT', action.type);
     switch (action.type) {
       default:
         return state;
@@ -69,28 +67,371 @@
     })
   );
 
-  // Create chat box wrapper
-  class Chat extends React.Component {
+  // React Components
+
+  class Notification extends React.Component {
     render () {
       return (
-        <div id="lets-chat" className="wrapper">
-          <Messages data={this.props.data} />
+        <ul className="letschat-alert" style={{margin: 0, listStyle: 'none'}}>
+        </ul>
+      )
+    }
+  }
+
+  class Status extends React.Component {
+    constructor (props) {
+      super(...arguments);
+
+      this.socket = props.socket;
+      this.socket.on('operator:typing', this.onTyping);
+
+      this.state = {
+        typing: false,
+        style: {}
+      };
+    }
+
+    onTyping () {
+      console.log('OPERATOR TYPING ...');
+    }
+
+    render () {
+      return (
+          <ul className="letschat-message-status" style={{
+            position: 'fixed',
+            bottom: '48px',
+            margin: 0,
+            boxSizing: 'border-box',
+            padding: '4px 8px',
+            height: '24px',
+            width: '265px',
+            fontSize: '13px',
+            fontFamily: 'sans-serif',
+            fontStyle: 'italic',
+            listStyle: 'none',
+            color: '#D1D1D1'
+          }}>
+            <li><span>John is typing...</span></li>
+          </ul>
+        )
+    }
+  }
+
+  const Message = (props) => {
+    return (
+      <li id="message_{props.id}" className="letschat-message">
+        <span className="author">
+
+        </span>
+        <span className="letschat-message-content">{props.children}</span>
+        <span className="letschat-message-timestamp">{props.timestamp}</span>
+      </li>
+    )
+  }
+
+  const messageListMapStateToProps = state => {
+    return {
+      messages: state.messages || []
+    };
+  }
+  const messageListMapDispatchToProps = dispatch => {
+    return {
+
+    };
+  }
+  const MessageList = (props) => {
+    let socket = props.socket;
+    let style = {
+      position: 'absolute',
+      top: '32px',
+      bottom: '48px',
+      left: 0,
+      right: 0,
+      overflowY: 'scroll',
+      borderRight: '1px solid #ccc'
+    };
+
+    let messages = props.messages.map((message) => {
+      return (
+        <Message id={message.id}>
+          {message.content}
+        </Message>
+      );
+    });
+
+    return (
+      <div className="letschat-messages" style={style}>
+        <div className="letschat-messages-wrapper" style={{position: 'relative', height: '100%'}}>
+          <Notification />
+          <ul className="letschat-message-list" style={{
+            margin: 0,
+            padding: 0,
+            listStyle: 'none',
+            fontSize: '14px',
+            fontFamily: 'sans-serif',
+            width: '100%'
+          }}>
+            {messages}
+          </ul>
+          <Status socket={socket} />
+        </div>
+      </div>
+    )
+  }
+
+  const Messages = connect(
+    messageListMapStateToProps,
+    messageListMapDispatchToProps
+  )(MessageList);
+
+
+  class Input extends React.Component {
+    constructor (props) {
+      super(props);
+
+      this.socket = props.socket;
+
+      // Initial state
+      this.state = {
+        style: {
+          position:' absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          width: '100%',
+          height: '48px',
+          boxSizing: 'border-box',
+          border: 0,
+          borderRight: '1px solid #ccc',
+          borderTop: '1px solid #ddd',
+          backgroundColor: '#fff',
+          padding: '8px',
+          color: '#222',
+          fontSize: '11px',
+          resize: 'none',
+          outline: 0
+        }
+      };
+    }
+
+
+    // Event Handler
+
+    onKeyPress (event) {
+      let key = event.key;
+      let keyCode = event.keyCode;
+      let shiftKey = event.shiftKey;
+      let ctrlKey = event.ctrlKey;
+      let altKey = event.altKey;
+
+      console.log('INPUT KEYPRESS', key, '(' + keyCode + ')', 'SHIFT', shiftKey, 'CTRL', ctrlKey, 'ALT', altKey);
+
+      if (keyCode == KEY_ENTER) {
+        if (!shiftKey) {
+          console.log('SENDING MESSAGE ...');
+          // Send data
+          this.socket.emit('client:message', event.target.value);
+
+          // Update message list
+          store.dispatch({ type: UI_HARD_ENTER,  message: event.target.value });
+        } else {
+          // Update input height
+          store.dispatch({ type: UI_SOFT_ENTER });
+        }
+      }
+
+      this.socket.emit('client:typing');
+    }
+
+
+    style (changes) {
+      if (changes) {
+        this.state.style = Object.assign({}, this.state.style, changes);
+      }
+
+      return this.state.style;
+    }
+
+    render () {
+      // console.log('RENDER', this);
+
+      return <textarea
+        id="letschat-input"
+        name="letschat-input"
+        type="text"
+        style={this.style()}
+        placeholder="Type a message&hellip;"
+        onKeyDown={this.onKeyPress.bind(this)} />
+    }
+  }
+
+  // Create chat box wrapper
+  class Chat extends React.Component {
+    constructor (props) {
+      super(props);
+
+      this.socket = io.connect(socketPath, {
+        reconnectionAttempts: 10
+      });
+
+      this.socket.on('connect', this.onSocketConnected);
+      this.socket.on('connect_error', this.onSocketConnectionError);
+      this.socket.on('connect_timeout', this.onSocketTimeout);
+      this.socket.on('disconnect', this.onSocketDisconnected);
+      this.socket.on('reconnect', this.onSocketReconnected);
+      this.socket.on('reconnecting', this.onSocketReconnecting);
+      // this.socket.on('reconnect_error', socketConnectionError);
+      this.socket.on('reconnect_failed', this.onSocketReconnectionFailed);
+      this.socket.on('reconnect_timeout', this.onSocketTimeout);
+      // this.socket.on('operator:message', this.handleOperatorMessage);
+
+      // Initial state
+      this.state = {
+        style: {
+          innerWrapper: {
+            display: 'block',
+            width: '280px',
+            height: '360px',
+            boxShadow: '#ddd 1px 1px 8px 0',
+            borderWidth: '0 1px 1px 0',
+            borderRadius: '3px 3px 0 0',
+            borderStyle: 'solid',
+            borderColor: '#ccc',
+            borderBottom: 0
+          },
+          outerWrapper: {
+            position: 'relative',
+            width: '80%',
+            margin: '0 auto'
+          },
+          box: {
+            position: 'absolute',
+            right: 0,
+            bottom: 0,
+            backgroundColor: '#ffffff'
+          },
+          header: {
+            display: 'block',
+            width: '100%',
+            height: '32px',
+            padding: '8px',
+            boxSizing: 'border-box',
+            borderRadius: '3px 3px 0 0',
+            boxShadow: '0 1px 1px 0 rgba(0,0,0,0.15)',
+            fontSize: '13px',
+            fontFamily: '\'Arial\', sans-serif',
+            color: 'white',
+            background: '#ef7f7f'
+          },
+          icon: {
+            float: 'right',
+            fontSize: '24px',
+            marginTop: '-5px',
+            fontWeight: '800',
+            cursor: 'pointer',
+            color: 'rgba(255,255,255,0.5)'
+          }
+        },
+        operator: {
+          firstName: 'John'
+        },
+        company: {
+          name: 'ACME'
+        }
+      };
+    }
+
+
+    // Event Handlers
+
+    // Successful connection
+    onSocketConnected () {
+      const state = store.getState();
+      console.log('DEBUG', 'Socket connected');
+
+      if (!state.chat.connected) {
+        store.dispatch({ type: CHAT_CONNECTED });
+      }
+    };
+
+    // Disconnected
+    onSocketDisconnected () {
+      const state = store.getState();
+      console.warn('DEBUG', 'Socket disconnected');
+
+      if (state.chat.connected) {
+        store.dispatch({ type: CHAT_DISCONNECTED });
+      }
+    };
+
+    // Successful re-connected
+    onSocketReconnected () {
+      console.log('DEBUG', 'Socket reconnected');
+    };
+
+    // Attempting to re-connect
+    onSocketReconnecting () {
+      console.log('DEBUG', 'Socket reconnecting ...');
+    };
+
+    // Failed to re-connect after manager.reconnectionAttempts tried
+    onSocketReconnectionFailed () {
+      console.error('DEBUG', 'Socket failed reconnection');
+    };
+
+    // Timeout either connecting or re-connecting
+    onSocketTimeout () {
+      console.warn('DEBUG', 'Socket timeout');
+    };
+
+    // Error when connecting or re-connecting
+    onSocketConnectionError () {
+      console.error('DEBUG', 'Socket connection error');
+    };
+
+
+    // Actions
+
+    open () {
+      console.log('DEBUG', 'Open chat');
+    }
+
+    close () {
+      console.log('DEBUG', 'Close chat');
+    }
+
+
+    style (changes) {
+      if (changes) {
+        this.state.style = Object.assign({}, this.state.style, changes);
+      }
+
+      return this.state.style;
+    }
+
+    render () {
+      let style = this.style();
+      let socket = this.socket;
+      let operator = this.state.operator;
+      let company = this.state.company;
+
+      return (
+        <div className="letschat-outer-wrapper" style={style.outerWrapper}>
+          <div className="letschat-box" style={style.box}>
+            <div className="letschat-inner-wrapper" style={style.innerWrapper}>
+              <div className="letschat-header" style={style.header}>
+                <span className="letschat-title"><strong>{operator.firstName}</strong>&nbsp;from&nbsp;{company.name}</span>
+                <i style={style.icon} onClick={this.close}>&#215;</i>
+              </div>
+              <Messages store={store} socket={socket} />
+              <Input socket={socket} />
+            </div>
+          </div>
         </div>
       )
     }
   }
 
-  class Messages extends React.Component {
-    render () {
-      return null;
-    }
-  }
-
-  class Input extends React.Component {
-    render () {
-      return null
-    }
-  }
 
   if (!React) {
     console.error('Required dependancy missing, React. https://facebook.github.io/react/downloads.html')
@@ -118,10 +459,8 @@
 
   // Our generic render function
   const render = function render () {
-    const state = store.getState();
-
     ReactDOM.render(
-      <Chat data={state.chat.messages} />,
+      <Chat store={store} />,
       document.getElementById('lets-chat')
     );
   };
@@ -132,23 +471,30 @@
     render();
   });
 
-
   // Initiate the socket connection
-  socket = io.connect('http://localhost:8000');
+  // socket = io.connect(socketPath, {
+  //   reconnectionAttempts: 10
+  // });
 
-  io.on('connect', socketConnected);
-  io.on('connect_error', socketConnectionError);
-  io.on('connect_timeout', socketTimeout);
-  socket.on('disconnect', socketDisconnected);
-  socket.on('reconnect', socketReconnected);
-  socket.on('reconnecting', socketReconnecting);
-  socket.on('reconnect_error', socketConnectionError);
-  socket.on('reconnect_failed', socketReconnectionFailed);
-  socket.on('reconnect_timeout', socketTimeout);
+  // let styles = document.createElement('link');
+  // styles.id = 'lets-chat-styles';
+  // styles.rel = 'stylesheet';
+  // styles.type = 'text/css';
+  // styles.media = 'all';
+  // styles.href = '/styles/lets-chat-syles-0.0.1.css';
 
   // Create our entry point
   let div = document.createElement('div');
-  div.id = "lets-chat";
+  div.id = 'lets-chat';
+  div.style.position = 'fixed';
+  div.style.bottom = 0;
+  div.style.right = 0;
+  div.style.left = 0;
+
   document.body.appendChild(div);
+  // document.body.insertBefore(styles, div);
+
+  // Start by going into disconnected mode (and then connecting)
+  store.dispatch({ type: CHAT_DISCONNECTED });
 
 }) (window)
