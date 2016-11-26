c406e74c92a4b4c2a2bb623decddb9bea1f1faf7
Matthew Mihok <4D.69.68.6F.6B@gmail.com>
2016-11-26 08:26:44 -0500

    Feature/test jest (#4)
    * moving to jest test framework
    * moving tests from __tests__ to test dir
    * fixing tests to not try to connect, removing logs
    * cleaning up test file

---
+++ test/script.test.js
@@ -0,0 +1,31 @@
+const React = global.window.React = require('react');
+const Redux = global.window.Redux = require('redux');
+const ReactTestUtils = global.window.ReactTestUtils = require('react-addons-test-utils');
+const ReactDOM = global.window.ReactDOM = require('react-dom');
+const ReactRedux = global.window.ReactRedux = require('react-redux');
+const ReactJSS = global.window.reactJss = require('react-jss');
+const io = global.window.io = require('socket.io-client');
+
+const { store, Message, MessageList, Input, Chat } = require('../src/script');
+
+
+// What do I want to test/make sure works with absolution
+// - redux ACTIONS are doing what they intend to do (both sides, firing and
+//  components reception)
+//    - UI_OPEN
+//    - UI_CLOSE
+//    - UI_STYLE_MESSANGER
+//    - UI_STYLE_MESSANGER
+//    - CHAT_CONNECTED
+//    - CHAT_DISCONNECTED
+//    - CHAT_MESSAGE_OPERATOR
+//    - CHAT_MESSAGE_CLIENT
+// - styling is available
+//
+
+test('state has a ui.style property', () => {
+  const state = store.getState();
+
+  expect(state.ui.hasOwnProperty('style')).toBe(true);
+  expect(state.ui.style).not.toBe(undefined);
+})
