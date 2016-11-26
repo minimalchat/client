ebee69c2fa31d81727ff015c3c62e82442f52618
Matthew Mihok <4D.69.68.6F.6B@gmail.com>
2016-11-26 14:19:49 -0500

    using css.js styles in the messange styling

--- src/css.js
+++ src/css.js
@@ -1,13 +1,25 @@
 let chat = {};
 chat.theme = {
-  avatar: true,
-  messageOperatorBackgroundColor: '',
-  messageUserBackgroundColor: '',
-  messageInputBackgroundColor: '',
-  chatPrimaryColor: '',
-  chatSecondaryColor: '',
-  chatPosition: {
-    x: '',
-    y: '',
-  },
+  // rtl: false,
+  // avatar: true,
+  // showTyping: true,
+  messageOperatorForegroundColor: '#000000',
+  messageOperatorBackgroundColor: undefined,
+  messageUserForegroundColor: '#ffffff',
+  messageUserBackgroundColor: '#0a6bef',
+  // messagesPadding: '',
+  messageListBackgroundColor: '#ffffff', // ignored for FLOAT
+  // messageListPadding: '',
+  messageInputForegroundColor: '#222222',
+  messageInputBackgroundColor: '#ffffff',
+  messageInputBorderColor: '#dddddd', // borderRight ignored for MESSANGER, all ignored for FLOAT
+  // messageInputPadding: '',
+  // corners: '',
+  chatForegroundColor: '#ffffff',
+  chatPrimaryColor: '#566b89',
+  chatSecondaryColor: '#ffffff',
+  // chatPosition: {
+  //   x: '',
+  //   y: '',
+  // },
 };
