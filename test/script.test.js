ad55d053cfd62fb7659662b411fbd26837350b37
Matthew Mihok <4D.69.68.6F.6B@gmail.com>
2016-11-26 11:51:14 -0500

    adding tests for input

--- test/script.test.js
+++ test/script.test.js
@@ -5,12 +5,15 @@ const ReactRedux = global.window.ReactRedux = require('react-redux');
 const ReactJSS = global.window.reactJss = require('react-jss');
 const io = global.window.io = require('socket.io-client');
 
-const renderer = require('react-test-renderer');
+const snapshot = require('react-test-renderer');
+const { shallow, mount, render } = require('enzyme');
 
 // Mock the Socket.io connect function for all tests
-io.connect = jest.fn(() => ({on: jest.fn()}));
-
-const { store, Message, MessageList, Input, Chat } = require('../src/script');
+const socket = {
+  on: jest.fn(),
+  emit: jest.fn()
+};
+io.connect = jest.fn(() => socket);
 
 // What do I want to test/make sure works with absolution
 // - redux ACTIONS are doing what they intend to do (both sides, firing and
@@ -25,10 +28,12 @@ const { store, Message, MessageList, Input, Chat } = require('../src/script');
 //    - CHAT_MESSAGE_CLIENT
 // - styling is available in state
 // - ?
-//
+
 
 describe('state', () => {
   it('has a ui.style property', () => {
+    const { store } = require('../src/script');
+
     const state = store.getState();
 
     expect(state.ui.hasOwnProperty('style')).toBe(true);
@@ -37,24 +42,99 @@ describe('state', () => {
   });
 });
 
-describe('Message', () => {});
+describe('Message', () => {
+  it('matches snapshot', () => {
+    const { Message } = require('../src/script');
 
-describe('MessageList', () => {});
+    const message = {
+      key: 0,
+      author: 'Test User',
+      content: ['Foo Bar']
+    };
 
-describe('Messages', () => {});
-
-describe('Input', () => {});
-
-describe('Chat', () => {
-  it('starts a socket connection', () => {
-    const component = renderer.create(
-      <Chat store={store} />
-    );
+    const component = snapshot.create(<Message key={message.key} author={message.author} content={message.content} />);
 
     let tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('MessageList', () => {
+  it('matches snapshot', () => {
+    const { MessageList } = require('../src/script');
+
+    const component = snapshot.create(<MessageList socket={socket} />);
+
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Input', () => {
+  afterEach(() => {
+    socket.emit.mockReset();
+  });
+
+  it('matches snapshot', () => {
+    const { Input } = require('../src/script');
+
+    const component = snapshot.create(<Input socket={socket} />);
+
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('emits a client:typing event on keyDown', () => {
+    const { Input } = require('../src/script');
+
+    const component = mount(<Input socket={socket} />);
+
+    component.find('textarea').simulate('keyDown', {
+      keyCode: 84,
+      key: "t"
+    });
+
+    expect(socket.emit).toHaveBeenCalled();
+    expect(socket.emit).toHaveBeenCalledWith('client:typing');
+  });
+
+
+  it('emits a client:message event on keyDown', () => {
+    const { store, Input } = require('../src/script');
+
+    const component = mount(<Input socket={socket} />);
+
+    store.dispatch = jest.fn();
+
+    component.find('textarea').simulate('keyDown', {
+      keyCode: 13,
+      shiftKey: false,
+      key: "Enter"
+    });
+
+    expect(socket.emit).toHaveBeenCalledTimes(2);
+    expect(socket.emit.mock.calls[0][0]).toBe('client:message');
+  });
+});
+
+describe('Chat', () => {
+  it('matches snapshot', () => {
+    const { store, Chat } = require('../src/script');
+
+    const component = snapshot.create(<Chat store={store} />);
+
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('starts a socket connection', () => {
+    const { store, Chat } = require('../src/script');
 
     expect(io.connect).toHaveBeenCalled();
   })
+
 });
