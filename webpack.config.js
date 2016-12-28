4d75d58fc38b3c36ddb831cfce73bbc900ff4de9
teesloane <tylersloane@gmail.com>
2016-12-27 21:36:47 -0500

    set up webpack.
    This minimal webpack config allows es6 imports / exports (as far as I can tell)
    
    It also spits out a compiled "scripts" bundle into the `dist` folder. This can be changed easily.
    
    The command to get webpack running is currently: `npm run dev`
    
    There is a possibility of using webpack-dev-server to enable hot reloading, but I'm not sure that makes sense since the app doesn't actually have an index.html as far as I can tell.
    
    ### resources ###
    webpack reference came from  - webpack base config from: https://gist.github.com/learncodeacademy/25092d8f1daf5e4a6fd3
    
    video I watched for getting some insight - video reference: https://www.youtube.com/watch?v=9kJVYpOqcVU

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var development = process.env.NODE_ENV !== "production";
+
+var PATHS = {
+  BUILD: __dirname + '/dist',
+  APP: __dirname + '/src'
+}
+
+module.exports = {
+  context: __dirname,
+  devtool: development ? "inline-sourcemap" : null,
+  entry: PATHS.APP + "/index.js",
+  output: {
+    path: PATHS.BUILD,
+    filename: "scripts.js"
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : PATHS.APP,
+        loader : 'babel'
+      }
+    ]
+  },
+  plugins: development ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
