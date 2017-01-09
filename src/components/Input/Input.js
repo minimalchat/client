ca6f9eb9f4b45e628979b0c3481a1040c4b818bf
teesloane <tylersloane@gmail.com>
2017-01-09 00:00:32 -0500

    Set up local state to create a controlled form input
    Generally, forms should be of "controlled" nature, in which their value is
    decided via state, rather than existing in the DOM. I changed the chat box to
    rely on the the state. For some reason, onKeyDown wouldn't fire off for handling state changes (I guess just onChange)
    
    details on controlled forms: https://facebook.github.io/react/docs/forms.html#controlled-components

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,8 +4,6 @@ import './Input_styles.css'
 import {updateMessageList} from '../../containers/Chat/actions'
 
 
-
-
 // TODO: move to a constants file? 
 const KEY_ENTER = 13;
 
@@ -18,32 +16,31 @@ class Input extends Component {
     },
   }
 
+
   constructor (props) {
     super(props);
     this.socket = props.socket;
+
+    this.state = { messageBox: '', }
   }
+
+  handleChange = (e) => { this.setState({messageBox: e.target.value}) }
 
   onKeyPress = (event) => {
     const {key, keyCode, shiftKey, ctrlKey, altKey, input} = event;
-    // console.log(`INPUT KEYPRESS ${key} (${keyCode}), SHIFT ${shiftKey}, CTRL ${ctrlKey}, ALT ${altKey}`);
+    console.log(`INPUT KEYPRESS ${key} (${keyCode}), SHIFT ${shiftKey}, CTRL ${ctrlKey}, ALT ${altKey}`);
 
     if (keyCode === KEY_ENTER) {
       if (!shiftKey) {
         console.log('SENDING MESSAGE ...');
         // Send data
         this.socket.emit('client:message', event.target.value);
-
-        // Update message list
         this.props.dispatch(updateMessageList(event.target.value));
-        // this.props.dispatch({ type: UI_HARD_ENTER, message: event.target.value });
-        // this.props.dispatch({ type: CHAT_MESSAGE_CLIENT, message: event.target.value });
+        this.setState({messageBox: ''})
 
         event.preventDefault();
-        // TODO: figure out how to deal with this?
-        // redux form type stuff? Controlled input in component state?
-        // input.value = '';
       } else {
-        // Update input height
+        // TODO Update input height
         this.props.dispatch({ type: UI_SOFT_ENTER });
       }
     }
@@ -59,6 +56,8 @@ class Input extends Component {
         className="Input-input"
         placeholder="Type a message&hellip;"
         onKeyDown={this.onKeyPress}
+        onChange={this.handleChange}
+        value={this.state.messageBox}
       />
     );
   }
@@ -71,76 +70,3 @@ const mapDispatchToProps = dispatch => ({
 
 export default connect(mapStateToProps, mapDispatchToProps)(Input);
 
-// import React, { Component, PropTypes } from 'React';
-
-
-// // @injectSheet(style('Input'))
-// class Input extends Component {
-//   static propTypes = {
-//     socket: (props, propName) => {
-//       if (!(propName in props)) {
-//         throw new Error('socket must be set.');
-//       }
-//     },
-//     // sheet: (props, propName) => {
-//     //   if (!(propName in props)) {
-//     //     throw new Error('sheet must be set.');
-//     //   }
-//     // },
-//   }
-//   constructor (props) {
-//     super(props);
-
-//     this.socket = props.socket;
-//   }
-
-
-//   // Event Handler
-
-//   onKeyPress (event) {
-//     const key = event.key;
-//     const keyCode = event.keyCode;
-//     const shiftKey = event.shiftKey;
-//     const ctrlKey = event.ctrlKey;
-//     const altKey = event.altKey;
-//     const input = event.target;
-
-//     console.log(`INPUT KEYPRESS ${key} (${keyCode}), SHIFT ${shiftKey}, CTRL ${ctrlKey}, ALT ${altKey}`);
-
-//     if (keyCode === KEY_ENTER) {
-//       if (!shiftKey) {
-//         console.log('SENDING MESSAGE ...');
-//         // Send data
-//         this.socket.emit('client:message', event.target.value);
-
-//         // Update message list
-//         store.dispatch({ type: UI_HARD_ENTER, message: event.target.value });
-//         store.dispatch({ type: CHAT_MESSAGE_CLIENT, message: event.target.value });
-
-//         event.preventDefault();
-//         input.value = '';
-//       } else {
-//         // Update input height
-//         store.dispatch({ type: UI_SOFT_ENTER });
-//       }
-//     }
-
-//     this.socket.emit('client:typing');
-//   }
-
-
-//   render () {
-//     // const { sheet: { classes } } = this.props;
-
-//     return (
-//       <textarea
-//         type="text"
-//         className="Input-input"
-//         placeholder="Type a message&hellip;"
-//         onKeyDown={this.onKeyPress.bind(this)}
-//       />
-//     );
-//   }
-// }
-  
-// export default Input;
