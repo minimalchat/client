270850fbca28e79ba572ab79b426dbcf9178984d
teesloane <tylersloane@gmail.com>
2017-03-09 19:55:25 -0500

    Update build process docs
    - include /example site and accompanying docs.

---
+++ example/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function (req, res) {
+ res.sendFile(__dirname + '/index.html');
+});
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
