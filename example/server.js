72ff1994e2635d38dad1ce806dd15f07723ca917
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2017-06-25 14:55:27 -0400

    Fixing some stuff before we make a big PR

--- example/server.js
+++ example/server.js
@@ -15,8 +15,8 @@ app.get('/', function (req, res) {
 
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  console.log(`Example app listening on port ${process.env.PORT || 3000}!`);
 });
 
 
