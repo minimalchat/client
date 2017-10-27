b54baadcc62005ed36abafffc0ada056bba9ee45
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2017-10-27 18:05:29 -0400

    Trying to fix weird issue with snapshots

--- src/components/ThemeProvider/index.test.js
+++ src/components/ThemeProvider/index.test.js
@@ -3,9 +3,11 @@ import render from 'preact-render-to-string';
 
 import ThemeProvider from '.';
 
+
+
 describe('<ThemeProvider />', () => {
   it('should render ', () => {
-    const tree = render(<ThemeProvider />);
+    const tree = render(<ThemeProvider><span>Hello World</span></ThemeProvider>);
     expect(tree).toMatchSnapshot();
   });
 });
